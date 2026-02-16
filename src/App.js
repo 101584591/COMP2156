5f5858d89182ccb0db983a8996cc6590fd05236c
101584591 <101584591@georgebrown.ca>
2026-02-15 22:16:11 -0500

    Lab 05 SPA React setup working

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>COMP2156 - Lab 05</h1>
+      <p>SPA is running successfully.</p>
+    </div>
+  );
+}
